e1f3d8a09fa34cfd4bf505d9802778807c2da5f9
KetolaC <ketolac@clarkson.edu>
2022-07-15 14:13:48 -0400

    Connect method implemented and tested

--- OpenBCIAPI.py
+++ OpenBCIAPI.py
@@ -1,24 +1,21 @@
 # Imports
 from cyton import OpenBCICyton
 import serial
-import serial.tools.list_ports
+import serial.tools.list_ports as st
 
 class OpenBCIAPI:
 
     def __init__(self, num_ch=8, debug=False, daisy=False):
-        self.num_ch = num_ch
-        self.debug = debug
-        self.daisy = daisy
-        self.baud = 115200  # Default baud rate
-        self.com_port = None
-        self.serial = None
-
-        # TODO
-        # connect to UART using the connect method
+        self.num_ch = num_ch                                        # The number of channels in use
+        self.debug = debug                                          # Debug script enabled status
+        self.daisy = daisy                                          # Determines if the Daisy is attached
+        self.baud = 115200                                          # Default baud rate
+        self.com_port = None                                        # The COM port for UART
+        self.serial = None                                          # Serial connection to board
 
     def connect(self):
         if self.com_port is None:                                   # If no COM port is specified
-            ports = serial.tools.list_ports.comports()              # Get all open COM ports
+            ports = st.comports()                                   # Get all open COM ports
 
             for p in ports:                                         # Parse through open COM ports
                 s = serial.Serial(port=p[0], baudrate=self.baud,
@@ -31,12 +28,25 @@ class OpenBCIAPI:
                     self.com_port = p[0]                            # Save the COM port
                     self.serial = s                                 # Save the serial connection
                     if self.debug:
-                        print("OpenBCI port: %s" %(self.com_port))
+                        print("OpenBCI port: %s" % self.com_port)
                     break                                           # Exit the loop
 
-            if self.com_port is None:                               # If no port detected
-                print('No OpenBCI board detected.')                  # Notify user
+            if self.com_port is None:                               # If no board detected
+                print('No OpenBCI board detected.')                 # Notify user
                 s.close()                                           # Close loose connection
+
+        else:
+            self.serial = serial.Serial(port=self.com_port,
+                                        baudrate=self.baud,
+                                        timeout=10)                 # Open serial connection to COM port
+            self.serial.write(b'v')                                 # Request message from OpenBCI board
+            message = self.serial.readline()                        # Read incoming message
+
+            if b'OpenBCI' in message:                               # Check if OpenBCI board is detected
+                print('OpenBCI connection established.')            # Notify user if connection established
+            else:                                                   # If no board detected
+                print('No OpenBCI board detected.')                 # Notify user
+                self.serial.close()                                 # Close loose connection
 
     def disconnect(self):
         pass
@@ -69,15 +79,24 @@ class OpenBCIAPI:
         self.baud = baud
 
     def set_com_port(self, com):
-        if 'COM' not in com:
-            raise ValueError("Port names must be in the format of 'COMi', where i is an integer.")
-        self.com_port = com
+
+        if 'COM' not in com:                                        # Check if COM port given
+            raise ValueError("Port names must be in the format "
+                             "of 'COMi', where i is an integer.")   # Give feedback on correct data format
+
+        open_ports = [p[0] for p in st.comports()]                  # Get open ports
+
+        if com not in open_ports:                                   # Check that given port is open
+            raise ValueError("Selected COM port is not open.")      # Raise error for closed ports
+
+        self.com_port = com                                         # Set port to user's input
 
 
 if __name__ == '__main__':
 
     test = OpenBCIAPI()
     print(test.com_port)
-    # test.set_com_port('COM4')
+    test.set_com_port('COM3')
+    print(test.com_port)
     test.connect()
     print(test.com_port)
